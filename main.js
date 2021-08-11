906f29937ca4f11d3269d43274f7756fac231d3b
Gerard Oosterhof <gerard@script.nl>
2021-08-11 15:00:24 +0200

    Code Review

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-var app = new Vue ({
+const DICE_SIDES = 6
+
+const app = new Vue ({
     el: '#root',
     data: {
         throws: {}
     },
-
     methods: {
         randomNumbers() {
-            for (i = 0; i <=7; i++){
-                let randomNum = (Math.floor(Math.random() * 6)) + 1;
-                console.log(randomNum);
-                if(this.throws[randomNum] === undefined) {
-                    this.$set(this.throws, randomNum, 1);
-                } else {
-                    this.$set(this.throws, randomNum, ++this.throws[randomNum]);
-                }
+            for (let i = 0; i <=7; i++){
+                const randomNum = (Math.floor(Math.random() * DICE_SIDES)) + 1;
+                this.throws[randomNum]++
+                // console.log(randomNum);
+                // if(this.throws[randomNum] === undefined) {
+                //     this.$set(this.throws, randomNum, 1);
+                // } else {
+                //     this.$set(this.throws, randomNum, ++this.throws[randomNum]);
+                // }
             }
-
             console.log(this.throws);
-
         },
-
         resetThrows() {
             this.throws = {};
+            for (let side = 1; side <= DICE_SIDES; side++) {
+                this.$set(this.throws, side, 0)
+            }
         }
-    }
+    },
+    mounted() {
+        this.resetThrows()
+    },
 })
 
